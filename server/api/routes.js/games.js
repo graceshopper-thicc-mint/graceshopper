6c94da6837ea63ea6028b37b3d3acb7a38fc7aef
Michael Orman <michaelorman61@gmail.com>
2021-08-03 13:03:32 -0400

    Created express GET routes for /api/games

---
+++ server/api/routes.js/games.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const { Game } = require('../../db');
+
+// GET /api/games
+router.get('/games', async (req, res, next) => {
+  try {
+    const allGames = Game.findAll();
+    res.send(allGames);
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.get('/api/games/:gameId', async (req, res, next) => {
+  try {
+    const game = Game.findByPk(req.params.gameId);
+    res.send(game);
+  } catch (error) {
+    next(error);
+  }
+})
+
+module.exports = router;
