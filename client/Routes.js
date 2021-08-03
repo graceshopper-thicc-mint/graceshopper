3d851eefb7d69147eb99b6de290ca02630e1df5f
Haram Chang <harammchang@gmail.com>
2021-08-03 16:56:58 -0400

    fixed import

--- client/Routes.js
+++ client/Routes.js
@@ -4,7 +4,7 @@ import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
 import {me} from './store'
-
+import { AllGames } from "./components/AllGames"
 /**
  * COMPONENT
  */
@@ -27,6 +27,7 @@ class Routes extends Component {
         ) : (
           <Switch>
             <Route path='/' exact component={ Login } />
+            <Route path="/games" component={AllGames} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
           </Switch>
