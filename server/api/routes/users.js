677bbd7998af6eae587f8622fbc1a3f3d9f1a1b7
Haram Chang <harammchang@gmail.com>
2021-08-05 12:08:18 -0400

    fixed up cart routes based on db models

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { models: { User, Cart, CartLine }} = require('../../db')
+const { models: { User, CartLine }} = require('../../db')
 module.exports = router
 
 // GET /api/users
@@ -26,16 +26,61 @@ router.get("/:userId/cart", async (req, res, next) => {
         id: userId
       },
       include: {
-        model: Cart,
-        include: { model: CartLine }
+        model: CartLine
       }
     })
-    res.send(user.cart.cartlines)
+    res.send(user.cartlines)
   } catch (error) {
     next(error)
   }
 })
 
 // POST /api/users/:userId/cart ==> everytime they "add to cart"
+router.post("/:userId/cart", async (req, res, next) => {
+  try {
+    /*with the userid, find their associated cart.
+    then with that cart, associate it to cartlines, and then add a row with the new product*/
+    const response = await CartLine.create(req.body)
+    res.status(201).send(response)
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 // PUT /api/users/:userId/cart ==> if they want to edit their cart
+router.put("/:userId/cart/:gameId", async (req, res, next) => {
+  try {
+    const userId = req.params.userId
+    const gameId = req.params.ganmeId
+    const gameToUpdate = await User.findOne({
+      where: {
+        id: userId
+      },
+      include: {
+        model: CartLine,
+        where: {
+          gameId
+        }
+      }
+    })
+    res.send(await CartLine.update(req.body))
+  } catch (error) {
+    next(error)
+  }
+})
+
+// DELETE /api/users/:userId/cart/:gameId ==> if they change the quantity down to 0, or if they click remove in their cart.
+router.delete("/:userId/cart/:gameId", async (req, res, next) => {
+  try {
+    const gameId = req.params.gameId
+    const gameToDelete = await CartLine.findOne({
+      where: {
+        gameId
+      }
+    })
+    await gameToDelete.destroy()
+    res.send(gameToDelete)
+  } catch (error) {
+    next(error)
+  }
+})
