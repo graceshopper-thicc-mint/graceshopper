4c1889a5613f1451aeb1dbfd59ef84510386f1be
Michael Orman <michaelorman61@gmail.com>
2021-08-05 11:54:55 -0400

    Closes #20

--- server/db/index.js
+++ server/db/index.js
@@ -5,37 +5,41 @@ const db = require("./db");
 const User = require("./models/User");
 const Game = require("./models/Game");
 const CartLine = require("./models/CartLine");
-const Cart = require("./models/Cart");
 const BillingInfo = require("./models/BillingInfo");
 const Invoice = require("./models/Invoice");
 const InvoiceLine = require("./models/InvoiceLine");
 
 //associations could go here!
 //manytomany for cart to game, game to cart
-Game.belongsToMany(Cart, { through: CartLine});
-Cart.belongsToMany(Game, { through: CartLine});
+Game.hasOne(CartLine);
+CartLine.belongsTo(Game);
 
-Invoice.hasOne(BillingInfo);
-BillingInfo.belongsTo(Invoice);
-
-User.hasOne(Cart);
-Cart.belongsTo(User);
+User.hasMany(CartLine);
+CartLine.belongsTo(User);
 
 User.hasOne(BillingInfo);
 BillingInfo.belongsTo(User);
 
 User.hasMany(Invoice);
-Invoice.belongsToMany(User, { through: "UserInvoice"});
+Invoice.belongsTo(User);
 
-Game.belongsToMany(Invoice, { through: InvoiceLine});
-Invoice.belongsToMany(Game, { through: InvoiceLine});
+Invoice.hasMany(InvoiceLine);
+InvoiceLine.belongsTo(Invoice);
 
+Game.hasOne(InvoiceLine);
+InvoiceLine.belongsTo(Game);
+
+BillingInfo.hasOne(Invoice);
+Invoice.belongsTo(BillingInfo);
+
+// Game.belongsToMany(Invoice, { through: InvoiceLine});
+// Invoice.belongsToMany(Game, { through: InvoiceLine});
 module.exports = {
   db,
   models: {
     User,
     Game,
-    Cart,
+    CartLine,
     BillingInfo
   },
 };
