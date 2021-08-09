f09c0a47b77e28ec231b6d8ef4cce94dbdc1d0fa
Haram Chang <harammchang@gmail.com>
2021-08-09 19:13:24 -0400

    deleted sendgrid.env

--- client/components/OrderConfirmation.js
+++ client/components/OrderConfirmation.js
@@ -12,7 +12,7 @@ class OrderConfirmation extends React.Component {
   async componentDidMount() {
     const { userId } = this.props
     const { data } = await axios.get(`/api/users/${userId}/purchases`)
-    const recentPurchase = data[0]
+    const recentPurcqhase = data[0]
     const number = recentPurchase.confirmationNumber
     this.setState({
       confirmationNumber: number
