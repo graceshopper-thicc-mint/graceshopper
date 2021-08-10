f404922085ae5defa970130d521663c3e97c539f
Sakib Hossain <sakiba09@gmail.com>
2021-08-10 11:52:38 -0400

    npm install cors

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,14 +1,20 @@
-import { getConfirmation } from 'history/DOMUtils';
-import { Redirect, Link, useHistory } from "react-router-dom"
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import CartSingleItem from './CartSingleItem';
-import OrderConfirmation from "./OrderConfirmation"
-import axios from "axios"
-import { fetchCart, updateCartInvoice, createNewCart } from '../store/cart';
+import { getConfirmation } from "history/DOMUtils";
+import { Redirect, Link, useHistory } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import CartSingleItem from "./CartSingleItem";
+import OrderConfirmation from "./OrderConfirmation";
+import axios from "axios";
+import { fetchCart, updateCartInvoice, createNewCart } from "../store/cart";
 
-const Cart = ({ cart, fetchCart, updateCartInvoice, createNewCart, userId }) => {
-  let history = useHistory()
+const Cart = ({
+  cart,
+  fetchCart,
+  updateCartInvoice,
+  createNewCart,
+  userId,
+}) => {
+  let history = useHistory();
   // this.state
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalGames, setTotalGames] = useState(0);
@@ -40,32 +46,31 @@ const Cart = ({ cart, fetchCart, updateCartInvoice, createNewCart, userId }) => 
   // create a new invoice for the user DONE
   // fetch that orderconfirmation number with the invoice id, I guess?
   // render out the orderconfirmation page
-   async function handleCheckout() {
-    const orderConfirmationNumber = Math.floor(Math.random() * 10000000)
-    const datePurchased = Date.now()
+  async function handleCheckout() {
+    const orderConfirmationNumber = Math.floor(Math.random() * 10000000);
+    const datePurchased = Date.now();
     if (!localStorage.token) {
       const { data } = await axios.post("/api/guests/invoice", {
         confirmationNumber: orderConfirmationNumber,
-        datePurchased
-      })
-      const confirmationNumber = data.confirmationNumber
+        datePurchased,
+      });
+      const confirmationNumber = data.confirmationNumber;
       history.push({
         pathname: `/confirmation`,
-        state: { confirmationNumber: orderConfirmationNumber}
-      })
-
+        state: { confirmationNumber: orderConfirmationNumber },
+      });
     } else {
-      await updateCartInvoice(orderConfirmationNumber, datePurchased)
+      await updateCartInvoice(orderConfirmationNumber, datePurchased);
       for (let key in localStorage) {
         if (key !== "token") {
-          localStorage.removeItem(key)
+          localStorage.removeItem(key);
         }
       }
-      await createNewCart()
+      await createNewCart();
       history.push({
-        pathname: `/users/${userId}/confirmation`
-      })
-      }
+        pathname: `/users/${userId}/confirmation`,
+      });
+    }
   }
 
   return (
@@ -73,9 +78,7 @@ const Cart = ({ cart, fetchCart, updateCartInvoice, createNewCart, userId }) => 
       <h3>Cart</h3>
       <div>
         {cart.map((game) => {
-          return (
-            <CartSingleItem key={game.id} game={game} />
-          );
+          return <CartSingleItem key={game.id} game={game} />;
         })}
       </div>
       <div>
@@ -88,21 +91,22 @@ const Cart = ({ cart, fetchCart, updateCartInvoice, createNewCart, userId }) => 
       </div>
     </div>
   );
-}
+};
 
 const mapStateToProps = (state) => {
   return {
     cart: state.cart,
-    userId: state.auth.id
-  }
-}
+    userId: state.auth.id,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchCart: () => dispatch(fetchCart()),
-    updateCartInvoice: (orderConfirmationNumber, datePurchased) => dispatch(updateCartInvoice(orderConfirmationNumber, datePurchased)),
-    createNewCart: () => dispatch(createNewCart())
-  }
-}
+    updateCartInvoice: (orderConfirmationNumber, datePurchased) =>
+      dispatch(updateCartInvoice(orderConfirmationNumber, datePurchased)),
+    createNewCart: () => dispatch(createNewCart()),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
